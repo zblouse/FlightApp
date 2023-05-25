2b0687799e540a324727286ee924e81f7463f19f
= <zachblouse@gmail.com>
2023-05-25 18:20:32 -0400

    last pojo

--- src/main/java/edu/psgv/sweng861/AmadeusFlightOffer.java
+++ src/main/java/edu/psgv/sweng861/AmadeusFlightOffer.java
@@ -15,4 +15,92 @@ public class AmadeusFlightOffer {
 	private List<Itinerary> itineraries;
 	private Dictionaries dictionaries;
 	
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	public String getSource() {
+		return this.source;
+	}
+	
+	public void setInstantTicketingRequired(boolean instantTicketingRequired) {
+		this.instantTicketingRequired = instantTicketingRequired;
+	}
+	
+	public boolean isInstantTicketingRequired() {
+		return this.instantTicketingRequired;
+	}
+	
+	public void setNonhomogenous(boolean nonhomogenous) {
+		this.nonhomogenous = nonhomogenous;
+	}
+	
+	public boolean isNonhomogenous() {
+		return this.nonhomogenous;
+	}
+	
+	public void setOneWay(boolean oneWay) {
+		this.oneWay = oneWay;
+	}
+	
+	public boolean isOneWay() {
+		return this.oneWay;
+	}
+	
+	public void setLastTicketingDate(String lastTicketingDate) {
+		this.lastTicketingDate = lastTicketingDate;
+	}
+	
+	public String getLastTicketingDate() {
+		return this.lastTicketingDate;
+	}
+	
+	public void setLastTicketingDateTime(String lastTicketingDateTime) {
+		this.lastTicketingDateTime = lastTicketingDateTime;
+	}
+	
+	public String getLastTicketingDateTime() {
+		return this.lastTicketingDateTime;
+	}
+	
+	public void setNumberOfBookableSeats(int numberOfBookableSeats) {
+		this.numberOfBookableSeats = numberOfBookableSeats;
+	}
+	
+	public int getNumberOfBookableSeats() {
+		return this.numberOfBookableSeats;
+	}
+	
+	public void setItineraries(List<Itinerary> itineraries) {
+		this.itineraries = itineraries;
+	}
+	
+	public List<Itinerary> getItineraries() {
+		return this.itineraries;
+	}
+	
+	public void setDictionaries(Dictionaries dictionaries) {
+		this.dictionaries = dictionaries;
+	}
+	
+	public Dictionaries getDictionaires() {
+		return this.dictionaries;
+	}
 }
