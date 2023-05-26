af52f37630f650953454ebd77c422f9bb322083a
= <zachblouse@gmail.com>
2023-05-26 19:30:29 -0400

    user input

--- src/main/java/edu/psgv/sweng861/Application.java
+++ src/main/java/edu/psgv/sweng861/Application.java
@@ -1,11 +1,20 @@
 package edu.psgv.sweng861;
 
+import java.util.Scanner;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AmadeusClient client = new AmadeusClient();
-		client.getFlights("MDT", "BOS", "2023-11-01", TravelClass.ECONOMY, 1, false);
+		Scanner in = new Scanner(System.in);
+		System.out.println("Starting Airport IATA Code: ");
+		String startingAirport = in.nextLine();
+		System.out.println("Ending Airport IATA Code: ");
+		String endingAirport = in.nextLine();
+		in.close();
+		client.getFlights(startingAirport, endingAirport, "2023-11-01", TravelClass.ECONOMY, 1, false);
+		
 	}
 
 }
